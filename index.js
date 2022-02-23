b0d552b9b3d649d3b38def018cc5262b7a5b38d1
Alex <alexandra.rieux@gmx.com>
2022-02-23 19:59:03 +0100

    Add function to fetch data and save memes into folder

--- index.js
+++ index.js
@@ -1,20 +1,85 @@
+import fs from 'node:fs';
+import axios from 'axios';
+import cheerio from 'cheerio';
+// import express from 'express';
+import https from 'node:https';
 import fetch from 'node-fetch';
+// import path from 'node:path';
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((text) => console.log(text));
+// const app = express();
+const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+const memes = [];
+const downloadedImgs = [];
 
-/*
+// Create a new memes folder for saving the downloaded images if none exists
+fs.mkdir('./memes', { recursive: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+});
 
-const http = require('https://memegen-link-examples-upleveled.netlify.app/');
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    // console.log(html);
 
-const server = http.createServer();
-server.on('request', (request, response) => {});
-*/
+    const $ = cheerio.load(html);
 
-// const fs = require('fs'); // Built-in filesystem package for Node.js
-// const fetch = require('node-fetch');
+    // Get images URLs and put them into an array
+    $('img').each((i, element) => {
+      const imageUrl = $(element).attr('src'); // Reference for the image element
+      memes.push(imageUrl);
+    });
+    // Taking first ten images and put them into new array
+    for (let i = 0; i < 10; i++) {
+      const images = memes[i];
+      downloadedImgs.push(images);
+    }
+    // Loop through the first 10 images and downloads them
+    for (let i = 0; i < 10; i++) {
+      // Add 0 before an image number if necessary
+      const path = i === 9 ? `./memes/${i + 1}.jpg` : `./memes/0${i + 1}.jpg`;
+      const file = fs.createWriteStream(path);
+      https.get(downloadedImgs[i], function (response) {
+        response.pipe(file);
+      });
+    }
 
-// const imageUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+    $.html();
+  })
+  .catch((err) => console.log(err));
 
-// fetch(imageUrl).then((res) => res.body.pipe(fs.createWriteStream('./memes')));
+// for (let i = 0; i < 10; i++) {
+//   fs.open(`./memes/0${i + 1}.jpg`, 'w', (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+//   const image = memes[i];
+//   fetch(image)
+//     .then((res) =>
+//       res.body.pipe(fs.createWriteStream(`.memes/0${i + 1}.jpg`)),
+//     )
+//     .catch((error) => {
+//       console.log(error);
+//     });
+
+//   console.log(image);
+// }
+
+//   const $ = cheerio.load(html);
+//   $('img', html).each(function () {
+//     const url = $(this).find('img').attr('src');
+//     memes.push({
+//       url,
+//     });
+//   });
+//   console.log(memes);
+// })
+// .catch((err) => console.log(err));
+
+//     // const memes = Array.from($('.meme-img'));
+//     function getImages(html) {
+//       const items = $('img');
+//       const list = [];
+//       const result = Array.from($('.meme-img'));
